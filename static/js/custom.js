aa5aeb984fd44e80ff99f83d8cb93e92a965a8cb
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-17 13:46:03 +0530

    home -m

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,170 +1,304 @@
-function setCookie(c_name, value, exdays){
-    $.cookie(c_name, value, {expires: exdays, path: "/"});
+function setCookie(c_name, value, exdays) {
+  $.cookie(c_name, value, { expires: exdays, path: "/" });
 }
 
-function isMobile(){
-    return navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/BlackBerry/);
+function isMobile() {
+  return (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/BlackBerry/)
+  );
 }
 
-function getJsonTexts(gidPage, lang){
-    var json = null;
-    $.ajax({
-        "async": false,
-        "global": false,
-        "url": "/backoffice/text/" + lang + ".json",
-        "dataType": "json",
-        "success": function(data){
-            json = data[gidPage];
-        }
-    });
-    return json;
+function getJsonTexts(gidPage, lang) {
+  var json = null;
+  $.ajax({
+    async: false,
+    global: false,
+    url: "/backoffice/text/" + lang + ".json",
+    dataType: "json",
+    success: function (data) {
+      json = data[gidPage];
+    },
+  });
+  return json;
 }
 
-function onCaptchaLoaded(){
-    var grecaptchav3_sitekey = "6LeYQwIaAAAAABTkK_for88BnawVhMQtA-yKgpx-";
+function onCaptchaLoaded() {
+  var grecaptchav3_sitekey = "6LeYQwIaAAAAABTkK_for88BnawVhMQtA-yKgpx-";
 
-    grecaptcha.render(
-        "recaptcha_container",
-        {
-            "sitekey": grecaptchav3_sitekey,
-            // "badge": "bottomleft",
-            "size": "invisible"
-        }
-    );
+  grecaptcha.render("recaptcha_container", {
+    sitekey: grecaptchav3_sitekey,
+    // "badge": "bottomleft",
+    size: "invisible",
+  });
 }
 
-$(document).on("ready", function(){
+$(document).on("ready", function () {
+  $(".js-close-cookies").click(function () {
+    setCookie("aviso", "1", 365);
+  });
+  $(".js-close-popup").click(function () {
+    setCookie("advice", "1", 1);
+  });
 
-    $(".js-close-cookies").click(function(){
-        setCookie("aviso", "1", 365);
-    });
-    $(".js-close-popup").click(function(){
-        setCookie("advice", "1", 1);
-    });
+  $(".js-close-popup").click(function () {
+    setCookie("pop-up", "1", 0.1);
+  });
 
+  $("form#news-form").submit(function (e) {
+    e.preventDefault();
+    console.log($(this).serialize());
+    var lang = $("#lang-input").val();
+    var tokens = getJsonTexts(0, lang);
+    var alertContent = "";
 
-    $(".js-close-popup").click(function(){
-        setCookie("pop-up", "1", 0.1);
-    });
+    if ($("input[name=nombre]").val().length < 2) {
+      alertContent += tokens[1201] + "\n";
+    }
 
+    if ($("input[name=apellidos]").val().length < 2) {
+      alertContent += tokens[1202] + "\n";
+    }
 
-    $('form#news-form').submit(function(e){
-       e.preventDefault();
-       console.log($(this).serialize());
-        var lang   = $("#lang-input").val();
-        var tokens = getJsonTexts(0, lang);
+    if ($("input[name=email]").val().length > 0) {
+      if (
+        !/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test($("input[name=email]").val())
+      ) {
+        alertContent += tokens[1203] + "\n";
+      }
+    }
+    if (!$("#conditions_news").is(":checked")) {
+      alertContent += tokens[1205] + "\n";
+    }
+
+    if (alertContent.length > 0) {
+      alertContent = tokens[1200] + "\n\n" + alertContent;
+      alert(alertContent);
+    } else {
+      var formData = new FormData($("#news-form").get(0));
+      $(".modal-loading").show();
+      $.ajax({
+        url: "/include/sendMail.php",
+        type: "POST",
+        data: formData,
+        success: function (data) {
+          $(".modal-loading").hide();
+          alert(tokens[1204]);
+          $("form#news-form").trigger("reset");
+        },
+        cache: false,
+        contentType: false,
+        processData: false,
+      });
+    }
+  });
+
+  $("form#contact-form").submit(function (e) {
+    e.preventDefault();
+
+    var lang = $("#lang-input").val();
+    var tokens = getJsonTexts(1171523235, lang);
+
+    grecaptcha.ready(function () {
+      grecaptcha.execute(0, { action: "send_contact" }).then(function (token) {
         var alertContent = "";
 
-        if($("input[name=nombre]").val().length < 2){
-            alertContent += tokens[1201] + "\n";
+        if ($("select[name=tipo_consulta]").val().length < 1) {
+          alertContent += tokens[6507] + "\n";
         }
 
-        if($("input[name=apellidos]").val().length < 2){
-            alertContent += tokens[1202] + "\n";
+        if ($("input[name=nombre]").val().length < 2) {
+          alertContent += tokens[6501] + "\n";
         }
 
-        if($("input[name=email]").val().length > 0){
-            if(!/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test($("input[name=email]").val())){
-                alertContent += tokens[1203] + "\n";
-            }
-        }
-        if(!$("#conditions_news").is(":checked")){
-            alertContent += tokens[1205] + "\n";
+        if ($("input[name=apellidos]").val().length < 2) {
+          alertContent += tokens[6502] + "\n";
         }
 
-        if(alertContent.length > 0){
-            alertContent = tokens[1200] + "\n\n" + alertContent;
-            alert(alertContent);
-        }else{
-
-            var formData = new FormData($("#news-form").get(0));
-            $(".modal-loading").show();
-            $.ajax({
-                url: "/include/sendMail.php",
-                type: "POST",
-                data: formData,
-                success: function(data){
-                    $(".modal-loading").hide();
-                    alert(tokens[1204]);
-                    $("form#news-form").trigger("reset");
-                },
-                cache: false,
-                contentType: false,
-                processData: false
-            });
+        if ($("input[name=email]").val().length > 0) {
+          if (
+            !/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test(
+              $("input[name=email]").val()
+            )
+          ) {
+            alertContent += tokens[6503] + "\n";
+          }
         }
+
+        if ($("input[name=telefono]").val() > 0) {
+          if (
+            !/((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/g.test(
+              $("input[name=telefono]").val()
+            )
+          ) {
+            alertContent += tokens[6504] + "\n";
+          }
+        }
+
+        if ($("textarea[name=mensaje]").val().length < 2) {
+          alertContent += tokens[6505] + "\n";
+        }
+
+        if (!$("#check_contact").is(":checked")) {
+          alertContent += tokens[6511] + "\n";
+        }
+
+        if (alertContent.length > 0) {
+          alertContent = tokens[6500] + "\n\n" + alertContent;
+          alert(alertContent);
+        } else {
+          // Añade token al formulario
+          $("form#contact-form").prepend(
+            '<input type="hidden" name="token" value="' + token + '">'
+          );
+          $("form#contact-form").prepend(
+            '<input type="hidden" name="action" value="send_contact">'
+          );
+          var formData = new FormData($("#contact-form").get(0));
+          $(".modal-loading").show();
+          $.ajax({
+            url: "/include/sendMail.php",
+            type: "POST",
+            data: formData,
+            success: function (data) {
+              $(".modal-loading").hide();
+              alert(tokens[6506]);
+              $("form#contact-form").trigger("reset");
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+          });
+        }
+      });
     });
+  });
 
+  const observer = new IntersectionObserver(
+    function (entries, observer) {
+      $.each(entries, function (index, entry) {
+        if (entry.isIntersecting) {
+          $(entry.target).addClass("in-view");
+          observer.unobserve(entry.target); // Stop observing once the animation is triggered
+        }
+      });
+    },
+    {
+      threshold: 0.1, // Trigger when at least 10% of the section is visible
+    }
+  );
 
-    $("form#contact-form").submit(function(e){
-        e.preventDefault();
+  $(".imgAnim").each(function () {
+    observer.observe(this);
+  });
 
-        var lang   = $("#lang-input").val();
-        var tokens = getJsonTexts(1171523235, lang);
+  var pageLocation = [],
+    lastPage = null;
+  $Zz = 0;
 
-        grecaptcha.ready(function(){
-            grecaptcha.execute(0, {action: "send_contact"}).then(function(token){
-                var alertContent = "";
+  // Centering elements
+  $(".centerClass").css({
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+  });
+  $(".pageWrapper").css({
+    left: "327px",
+    perspective: "1000px",
+  });
+  $(".page").css({
+    transformStyle: "preserve-3d",
+    position: "relative",
+  });
+  $(".back").css({
+    transform: "rotateY(-180deg)",
+  });
+  $(".back, .front").css({
+    backfaceVisibility: "hidden",
+  });
 
-                if($("select[name=tipo_consulta]").val().length < 1){
-                    alertContent += tokens[6507] + "\n";
-                }
+  // Page click event
+  $(".page").click(function () {
+    if (
+      pageLocation[this.id] === undefined ||
+      pageLocation[this.id] == "right"
+    ) {
+      $Zz = $(".left").length + 1;
+      $(this)
+        .css({
+          transform: "rotateY(-180deg)",
+          transformOrigin: "-1px top",
+          zIndex: $Zz,
+          z: $Zz,
+        })
+        .addClass("left")
+        .removeClass("right");
+      pageLocation[this.id] = "left";
+    } else {
+      $Zz = $(".right").length + 1;
+      $(this)
+        .css({
+          transform: "rotateY(0deg)",
+          transformOrigin: "left top",
+          zIndex: $Zz,
+          z: $Zz,
+        })
+        .addClass("right")
+        .removeClass("left");
+      pageLocation[this.id] = "right";
+    }
+  });
 
-                if($("input[name=nombre]").val().length < 2){
-                    alertContent += tokens[6501] + "\n";
-                }
+  // Front hover event
+  $(".front").hover(
+    function () {
+      $(this).find(".pageFoldRight").animate(
+        {
+          width: "50px",
+          height: "50px",
+          backgroundImage:
+            "linear-gradient(45deg,  #fefefe 0%,#f2f2f2 49%,#ffffff 50%,#ffffff 100%)",
+        },
+        300
+      );
+    },
+    function () {
+      $(this).find(".pageFoldRight").animate(
+        {
+          width: "0px",
+          height: "0px",
+        },
+        300
+      );
+    }
+  );
 
-                if($("input[name=apellidos]").val().length < 2){
-                    alertContent += tokens[6502] + "\n";
-                }
-
-                if($("input[name=email]").val().length > 0){
-                    if(!/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/.test($("input[name=email]").val())){
-                        alertContent += tokens[6503] + "\n";
-                    }
-                }
-
-                if($("input[name=telefono]").val() > 0){
-                    if(!/((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/g.test($("input[name=telefono]").val())){
-                        alertContent += tokens[6504] + "\n";
-                    }
-                }
-
-                if($("textarea[name=mensaje]").val().length < 2){
-                    alertContent += tokens[6505] + "\n";
-                }
-
-                if(!$("#check_contact").is(":checked")){
-                    alertContent += tokens[6511] + "\n";
-                }
-
-                if(alertContent.length > 0){
-                    alertContent = tokens[6500] + "\n\n" + alertContent;
-                    alert(alertContent);
-                }else{
-                    // Añade token al formulario
-                    $("form#contact-form").prepend("<input type=\"hidden\" name=\"token\" value=\"" + token + "\">");
-                    $("form#contact-form").prepend("<input type=\"hidden\" name=\"action\" value=\"send_contact\">");
-                    var formData = new FormData($("#contact-form").get(0));
-                    $(".modal-loading").show();
-                    $.ajax({
-                        url: "/include/sendMail.php",
-                        type: "POST",
-                        data: formData,
-                        success: function(data){
-                            $(".modal-loading").hide();
-                            alert(tokens[6506]);
-                            $("form#contact-form").trigger("reset");
-                        },
-                        cache: false,
-                        contentType: false,
-                        processData: false
-                    });
-                }
-            });
-        });
-    });
-
-
-
+  // Back hover event
+  $(".back").hover(
+    function () {
+      $(this).find(".pageFoldLeft").animate(
+        {
+          width: "50px",
+          height: "50px",
+          backgroundImage:
+            "linear-gradient(135deg,  #ffffff 0%,#ffffff 50%,#f2f2f2 51%,#fefefe 100%)",
+        },
+        300
+      );
+    },
+    function () {
+      $(this).find(".pageFoldLeft").animate(
+        {
+          width: "0px",
+          height: "0px",
+        },
+        300
+      );
+    }
+  );
 });
