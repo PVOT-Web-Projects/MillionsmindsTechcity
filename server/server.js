39214c801427b5944ccc74d511ec7b6dabb5ea2d
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-17 12:52:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3010;
 
 // Middleware
 app.use(bodyParser.json());
